a1dd7ba1b47a79c8d9f4c613c2f661713e930bcb
yusukeueda0430 <bikecaryou4846wy@gmail.com>
2020-09-28 15:29:32 +0900

    Issue6

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,7 +34,7 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      wday_num = Date.today.wday + X
+      wday_num = Date.today.wday + x
       #もしもwday_numが7以上であれば、7を引く
       if wday_num >=7
         wday_num = wday_num - 7
